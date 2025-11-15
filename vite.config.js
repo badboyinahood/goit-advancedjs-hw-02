649acf223129cc224a51b859eac7cb211ec5bf1e
Konstantin <65172983+badboyinahood@users.noreply.github.com>
2025-11-16 00:27:23 +0200

    123

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+
+// ⚠️ Название твоего репозитория
+const repo = "goit-advancedjs-hw-02";
 
 export default defineConfig({
   root: "src",
-  define: {
-    global: "window",
-  },
+
+  base: `/${repo}/`,
+
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "src/index.html"),
+        timer: resolve(__dirname, "src/1-timer.html"),
+        snackbar: resolve(__dirname, "src/2-snackbar.html"),
+      },
+    },
   },
 });
