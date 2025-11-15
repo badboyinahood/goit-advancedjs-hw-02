5c7bb64248cf6fc311a8aee45bfdf438ffab106f
Konstantin <65172983+badboyinahood@users.noreply.github.com>
2025-11-15 23:57:12 +0200

    123

--- vite.config.js
+++ vite.config.js
@@ -1,59 +1,12 @@
 import { defineConfig } from "vite";
-import { glob } from "glob";
-import injectHTML from "vite-plugin-html-inject";
-import FullReload from "vite-plugin-full-reload";
-import postcssSortMediaQueries from "postcss-sort-media-queries";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   root: "src",
-
-  // ⚠️ ОБЯЗАТЕЛЬНО: поменяй goit-js-hw-10 на реальное имя репозитория
-  base: "/goit-advancedjs-hw-02/",
-
   define: {
-    // фикс ошибки global is not defined только в dev
-    ...(command === "serve" ? { global: {} } : {}),
+    global: "window",
   },
-
   build: {
     outDir: "../dist",
     emptyOutDir: true,
-    sourcemap: true,
-
-    rollupOptions: {
-      // Обрабатываем все HTML в src/
-      input: glob.sync("./src/*.html"),
-
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) return "vendor";
-        },
-
-        entryFileNames: chunk => {
-          if (chunk.name === "commonHelpers") return "commonHelpers.js";
-          return "[name].js";
-        },
-
-        assetFileNames: asset => {
-          if (asset.name && asset.name.endsWith(".html")) {
-            return "[name].[ext]";
-          }
-          return "assets/[name]-[hash][extname]";
-        },
-      },
-    },
   },
-
-  plugins: [
-    injectHTML(),
-    FullReload(["./src/**/*.html"]),
-  ],
-
-  css: {
-    postcss: {
-      plugins: [
-        postcssSortMediaQueries({ sort: "mobile-first" }),
-      ],
-    },
-  },
-}));
+});
